6fda6b5423fe5e8f94306fe1cfec4fb48f781bc8
Joan Amudu <joan.baare@gmail.com>
2021-11-10 11:38:21 +0000

    Started products template

--- products/views.py
+++ products/views.py
@@ -7,11 +7,10 @@ from .models import Product
 def all_products(request):
     """ A view to display all products, including sorting and search queries """
 
-    products = Product.object.all()
+    products = Product.objects.all()
 
     context = {
         'products': products,
     }
-
 
     return render(request, 'products/products.html', context)
