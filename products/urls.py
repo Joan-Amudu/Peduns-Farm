e1c5eacb6e3d3de9e82d502e25b69afacfc17d8f
Joan Amudu <joan.baare@gmail.com>
2021-11-10 12:13:42 +0000

    Updated products description, and added product detail functionality

--- products/urls.py
+++ products/urls.py
@@ -4,4 +4,6 @@ from .import views
 # Create your urls here.
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
+
 ]
