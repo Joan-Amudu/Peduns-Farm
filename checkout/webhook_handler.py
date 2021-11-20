df537503ed93013a3091c4655b3c7420969f632c
Joan Amudu <joan.baare@gmail.com>
2021-11-20 19:36:03 +0000

    Added calss methods to webhook handler and webhook views

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -10,5 +10,17 @@ class StripeWH_Handler:
     def handle_event(self, event):
         """Handle a generic/unknown/unexpected webhook event"""
         return HttpResponse(
+            content=f'Unhandled Webhook Recieved: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """Handle the payment_intent_succeeded webhook from stripe"""
+        return HttpResponse(
+            content=f'Webhook Recieved: {event["type"]}',
+            status=200)
+    
+    def handle_payment_intent_payment_failed(self, event):
+        """Handle the payment_intent.payment_failed webhook from stripe"""
+        return HttpResponse(
             content=f'Webhook Recieved: {event["type"]}',
             status=200)
