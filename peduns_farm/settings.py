20a7c42c0586a370e3fd4137798905ee5ce606bc
Joan Amudu <joan.baare@gmail.com>
2021-11-23 17:47:37 +0000

    Updated Country field in Checkout form

--- peduns_farm/settings.py
+++ peduns_farm/settings.py
@@ -188,7 +188,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Configurations
     AWS_STORAGE_BUCKET_NAME = 'peduns-farm'
     AWS_S3_REGION_NAME = 'eu-north-1'
