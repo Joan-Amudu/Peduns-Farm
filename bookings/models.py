af4569caad2312442c70bc73a5d23a432aea9720
Joan Amudu <joan.baare@gmail.com>
2021-11-29 15:35:45 +0000

    User Booking: Updated booking models

--- bookings/models.py
+++ bookings/models.py
@@ -10,10 +10,24 @@ class UserBooking(models.Model):
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=50, null=False, blank=False)
-    email = models.EmailField(max_length=254, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)    
     number_of_people = models.CharField(max_length=20, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    date = models.DateTimeField(auto_now_add=True)
+    date = models.CharField(max_length=20, null=False, blank=False)
+
+    def _generate_booking_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the booking number
+        if it hasn't been set already.
+        """
+        if not self.booking_number:
+            self.booking_number = self._generate_booking_number()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.user.username
