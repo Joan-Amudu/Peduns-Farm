a629a4bfc98cccf6fae19dc44ada46a0f25d3387
Joan Amudu <joan.baare@gmail.com>
2021-11-29 14:30:31 +0000

    User Booking: Added confirmation modal and deleted date picker

--- bookings/models.py
+++ bookings/models.py
@@ -9,11 +9,11 @@ class UserBooking(models.Model):
     User booking model for managing fruit picking bookings
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_full_name = models.CharField(max_length=50, null=False, blank=False)
-    default_email = models.EmailField(max_length=254, null=False, blank=False)
-    default_number_of_people = models.CharField(max_length=20, null=False, blank=False)
-    default_phone_number = models.CharField(max_length=20, null=False, blank=False)
-    default_date = models.DateTimeField(auto_now_add=True)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    number_of_people = models.CharField(max_length=20, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.user.username
--- bookings/views.py
+++ bookings/views.py
@@ -5,7 +5,16 @@ from django.shortcuts import render
 
 def booking(request):
     """User fruit picking bookings"""
+
+    if request.method == 'POST':        
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],            
+            'date': request.POST['date'],
+        }    
+
     template = 'bookings/booking.html'
     context = {}
 
     return render(request, template, context)
+   
