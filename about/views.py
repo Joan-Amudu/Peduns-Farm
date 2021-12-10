8b73def0fbafd82d7878cbb44f81385ff8773702
Joan Amudu <joan.baare@gmail.com>
2021-12-10 00:04:17 +0000

    About Us: Recommended products shuffled in views and limited to 4

--- about/views.py
+++ about/views.py
@@ -1,16 +1,18 @@
+import random
 from django.shortcuts import render
-
 from products.models import Product
 
 # Create your views here.
 
 
 def about(request):
-    """ 
+    """
     A view to return about us page with recommended products section
-    limit to only 4 products
-    """   
-    products = Product.objects.all()[0:4] 
+    Recommended Products are shuffled 
+    """
+
+    products = list(Product.objects.all())
+    random.shuffle(products) 
 
     context = {
         'products': products,
